844b51cf1f7a07f108e0c107fdb436f91caab211
putrobudi <putromulyobudi@gmail.com>
2021-10-08 21:38:14 +0000

    Caching

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import _ from 'lodash';
-import printMe from './print.js';
+// import Print from './print';
 
 function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+    const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.onclick = Print.bind(null, 'Hello webpack!');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-
-
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
 
-  entry: {
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
 
-    index: './src/index.js',
-
-    print: './src/print.js',
-
-
-  },
-
-  plugins: [
-
-    new HtmlWebpackPlugin({
-
-      title: 'Output Management',
-
-    }),
-
-  ],
-
-  output: {
-
-    filename: '[name].bundle.js',
-
-    path: path.resolve(__dirname, 'dist'),
-
-    clean: true
-
-  },
-
-
-
-
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
 };
