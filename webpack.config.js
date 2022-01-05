126ddd4d444cf9b99868a09ceb704211a9898675
putrobudi <putromulyobudi@gmail.com>
2022-01-05 07:18:48 +0000

    Add note about print.js as a lazy loading or not when being seperated as another src from index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-    },
+  entry: {
+    index: "./src/index.js",
+    // print: './src/print.js' /* // having knowing the answer to multiple vs single src (check JS Fundamentals note), does this count as lazy loading? Hmm, probably. Because you need to click the button to see the message. */
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-        }),
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
 
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        moduleIds: 'deterministic',
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
         },
+      },
     },
+  },
 };
